376edcd3662cf09a3e7961ce70406ed65f977148
beyondthwisdom <139395290+beyondthwisdom@users.noreply.github.com>
2024-03-24 08:42:36 +0000

    script groovy düzeltildi

--- script.groovy
+++ script.groovy
@@ -14,7 +14,7 @@ def testBuild(){
     withCredentials([usernamePassword(credentialsId: 'docker-credential', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
         sh 'echo $PASS | docker login -u $USER --password-stdin'
         sh 'echo Derleme ve image build ve push işlemi başlatılıyor'
-        sh './mvnw package -Pprod -DskipTests jib:build -Djib.to.image=btwdevops/jenkinsders5-test:pipeline-${params.VERSION}'
+        sh "./mvnw package -Pprod -DskipTests jib:build -Djib.to.image=btwdevops/jenkinsders5-test:pipeline-${params.VERSION}"
     }
 }
 
@@ -23,7 +23,7 @@ def prodBuild(){
     withCredentials([usernamePassword(credentialsId: 'docker-credential', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
         sh 'echo $PASS | docker login -u $USER --password-stdin'
         sh 'echo Derleme ve image build ve push işlemi başlatılıyor'
-        sh './mvnw package -Pprod -DskipTests jib:build -Djib.to.image=btwdevops/jenkinsders5-prod:pipeline-${params.VERSION}'
+        sh "./mvnw package -Pprod -DskipTests jib:build -Djib.to.image=btwdevops/jenkinsders5-prod:pipeline-${params.VERSION}"
     }
 
 }
