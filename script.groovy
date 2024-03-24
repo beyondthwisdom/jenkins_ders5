9e53912d74074e2b47dd2b2e844c5f1615afba4a
beyondthwisdom <139395290+beyondthwisdom@users.noreply.github.com>
2024-03-24 08:19:19 +0000

    script.groovy düzeltildi

--- script.groovy
+++ script.groovy
@@ -1,33 +1,30 @@
-class BuildScript {
 
-    def devBuild(){
-        sh "echo Dev Building..."
-        withCredentials([usernamePassword(credentialsId: 'docker-credential', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-            sh "echo $PASS | docker login -u $USER --password-stdin"
-            sh "echo Derleme ve image build ve push işlemi başlatılıyor"
-            sh "./mvnw package -Pprod -DskipTests jib:build -Djib.to.image=btwdevops/jenkinsders5-dev:pipeline-${params.VERSION}"
-        }
-    }
 
-    def testBuild(){
-        sh "echo Test Building..."
-        withCredentials([usernamePassword(credentialsId: 'docker-credential', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-            sh "echo $PASS | docker login -u $USER --password-stdin"
-            sh "echo Derleme ve image build ve push işlemi başlatılıyor"
-            sh "./mvnw package -Pprod -DskipTests jib:build -Djib.to.image=btwdevops/jenkinsders5-test:pipeline-${params.VERSION}"
-        }
-    }
-
-    def prodBuild(){
-        sh "echo Prod Building..."
-        withCredentials([usernamePassword(credentialsId: 'docker-credential', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-            sh "echo $PASS | docker login -u $USER --password-stdin"
-            sh "echo Derleme ve image build ve push işlemi başlatılıyor"
-            sh "./mvnw package -Pprod -DskipTests jib:build -Djib.to.image=btwdevops/jenkinsders5-prod:pipeline-${params.VERSION}"
-        }
-
+def devBuild(){
+    echo 'Dev Building...'
+    withCredentials([usernamePassword(credentialsId: 'docker-credential', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'echo Derleme ve image build ve push işlemi başlatılıyor'
+        sh './mvnw package -Pprod -DskipTests jib:build -Djib.to.image=btwdevops/jenkinsders5-dev:pipeline-${params.VERSION}'
     }
 }
 
-// Return an instance of the class to make it accessible from Jenkinsfile
-return new BuildScript()
+def testBuild(){
+    sh 'echo Test Building...'
+    withCredentials([usernamePassword(credentialsId: 'docker-credential', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'echo Derleme ve image build ve push işlemi başlatılıyor'
+        sh './mvnw package -Pprod -DskipTests jib:build -Djib.to.image=btwdevops/jenkinsders5-test:pipeline-${params.VERSION}'
+    }
+}
+
+def prodBuild(){
+    sh 'echo Prod Building...'
+    withCredentials([usernamePassword(credentialsId: 'docker-credential', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'echo Derleme ve image build ve push işlemi başlatılıyor'
+        sh './mvnw package -Pprod -DskipTests jib:build -Djib.to.image=btwdevops/jenkinsders5-prod:pipeline-${params.VERSION}'
+    }
+
+}
+return this
